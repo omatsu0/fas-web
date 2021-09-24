de0faf6c5520d5ea74f676d6812bc93447567e8e
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-09-24 22:10:42 +0900

    bootstrap fontawesome js関連動作確認

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: "posts#index"
   resources :posts
+
+  namespace :home do
+    get "about", as: 'about'
+    get "terms", as: 'terms'
+    get "privacy", as: 'privacy'
+  end
+  
 end
