21713b12045a3477d43133e4e242dfc476f5b35e
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-09-25 08:55:17 +0900

    トップ追加テスト

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '05d3ec2724993140ac4e3b77644360fb1219dbb624f1c717d333f675cad93bd51b368927255912a5bc706c25c4e711b4e7e174889cc23f0dd985ca068404a6fe'
