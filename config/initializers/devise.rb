d43bd997c4e462139a18fc33ae021c541f03c003
“omatsu0“ <62799261+omatsu0@users.noreply.github.com>
2021-10-02 10:15:38 +0900

    google認証redirect uri追加

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -309,5 +309,6 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:client_secret_id]
+  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], 
+  Rails.application.credentials.google[:client_secret_id],redirect_uri: "#{Rails.application.credentials.app[:host]}/users/auth/google_oauth2/callback"
 end
